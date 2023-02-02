dfeb8fafc0b290dfdab8b2e5c33948674cb3c55c
Nik Graf <nik@nikgraf.com>
2023-02-02 10:16:50 +0100

    chore: improve tests for crypto_box_keypair

--- example/src/components/tests/Test_crypto_box_keypair.tsx
+++ example/src/components/tests/Test_crypto_box_keypair.tsx
@@ -3,57 +3,28 @@ import {
   crypto_box_keypair,
   crypto_box_PUBLICKEYBYTES,
   crypto_box_SECRETKEYBYTES,
-  from_base64,
-  KeyPair,
-  StringKeyPair,
 } from 'react-native-libsodium';
-import type { OutputFormat } from 'src/types';
 import { FunctionStatus } from '../FunctionStatus';
 
-const lengthVerifies = (
-  key: any,
-  expectedLength: number,
-  outputFormat: OutputFormat
-) => {
-  const keyLength = key.length;
-  if (outputFormat === 'base64') {
-    return (
-      typeof key === 'string' && from_base64(key).length === expectedLength
-    );
-  } else if (outputFormat === 'hex') {
-    return typeof key === 'string' && keyLength === expectedLength * 2;
-  } else {
-    return typeof key === 'object' && keyLength === expectedLength;
-  }
-};
-
-const expectKeyPair = (
-  keyPair: KeyPair | StringKeyPair,
-  outputFormat: OutputFormat
-) => {
-  return (
-    keyPair.keyType === 'x25519' &&
-    lengthVerifies(
-      keyPair.publicKey,
-      crypto_box_PUBLICKEYBYTES,
-      outputFormat
-    ) &&
-    lengthVerifies(keyPair.privateKey, crypto_box_SECRETKEYBYTES, outputFormat)
-  );
-};
-
 export const Test_crypto_box_keypair: React.FC = () => {
+  const keyPair = crypto_box_keypair();
+  const keyPairBase64 = crypto_box_keypair('base64');
+
   return (
-    <>
-      <FunctionStatus
-        name="crypto_box_keypair"
-        success={
-          expectKeyPair(crypto_box_keypair(), 'uint8array') &&
-          expectKeyPair(crypto_box_keypair('uint8array'), 'uint8array') &&
-          expectKeyPair(crypto_box_keypair('base64'), 'base64') &&
-          expectKeyPair(crypto_box_keypair('hex'), 'hex')
-        }
-      />
-    </>
+    <FunctionStatus
+      name="crypto_box_keypair"
+      success={
+        keyPair.keyType === 'x25519' &&
+        keyPair.publicKey.length === crypto_box_PUBLICKEYBYTES &&
+        typeof keyPair.publicKey === 'object' &&
+        keyPair.privateKey.length === crypto_box_SECRETKEYBYTES &&
+        typeof keyPair.privateKey === 'object' &&
+        keyPairBase64.keyType === 'x25519' &&
+        keyPairBase64.publicKey.length === 43 &&
+        typeof keyPairBase64.publicKey === 'string' &&
+        keyPairBase64.privateKey.length === 43 &&
+        typeof keyPairBase64.privateKey === 'string'
+      }
+    />
   );
 };
