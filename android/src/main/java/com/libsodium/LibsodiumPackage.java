924d923194b7ac14ab804a4bfcaf6b5f8b9723d5
Nik Graf <nik@nikgraf.com>
2023-07-27 13:59:43 +0200

    feat: add android support

--- android/src/main/java/com/libsodium/LibsodiumPackage.java
+++ android/src/main/java/com/libsodium/LibsodiumPackage.java
@@ -7,17 +7,15 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 
 public class LibsodiumPackage implements ReactPackage {
   @NonNull
   @Override
   public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
-    List<NativeModule> modules = new ArrayList<>();
-    modules.add(new LibsodiumModule(reactContext));
-    return modules;
+    return Collections.singletonList(new LibsodiumModule(reactContext));
   }
 
   @NonNull
